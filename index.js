5f849048c37775506677deb70ca4ea62965d7cdb
DESKTOP-2RS8KHN\Cairo <kamaunewton78@gmail.com>
2022-08-15 09:43:10 +0300

    TODOs

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const itemsContainer = document.getElementById("itemsContainer");
 const itemObj = [
-{ name:"Finish coding challenge",price:1},{name:"get serious with amazon",price:100},{name:"Get to work",price:3}
-]
+    {
+        name: "Finish coding challenge", price: 1
+    },
+    { name: "get serious with amazon", price: 100 },
+    {
+        name: "Get to work", price: 3
+    }
+];
+// render the items we have for sale with an add item button
+// add to basket function - item appears in the basket with a remove button
+// remove from basket function - item disappears from the basket
+// total price function - total price of all items in the basket
