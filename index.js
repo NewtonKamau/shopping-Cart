f4dfee53c47823f2f6265abc6aa62630d12987ae
DESKTOP-2RS8KHN\Cairo <kamaunewton78@gmail.com>
2022-08-15 09:35:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const itemsContainer = document.getElementById("itemsContainer");
+const itemObj = [
+{ name:"Finish coding challenge",price:1},{name:"get serious with amazon",price:100},{name:"Get to work",price:3}
+]
