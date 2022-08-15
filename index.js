1b65b9aa1f217b6f9037caec42bd5ad4c8ffa2c9
DESKTOP-2RS8KHN\Cairo <kamaunewton78@gmail.com>
2022-08-15 11:00:41 +0300

    add to cart functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const itemsContainer = document.getElementById("itemsContainer");
+const shoppingCart = document.getElementById("shoppingCart");
 const itemObj = [
     {
         name: "Finish coding challenge", price: 1
@@ -11,17 +12,39 @@ const itemObj = [
 // render the items we have for sale with an add item button
 function render() {
     let itemsHTML = "";
-    for (let item of itemObj) { 
-        itemsHTML += `
+     itemObj.forEach(function (item, index) {
+       itemsHTML += `
         <div class="itemsElement">
             <p >${item.name} <span>${item.price}</span></p>
-            <button class="addButton">+</button>
+            <button class="addButton" id="${index}">+</button>
         </div>
         `;
-    }
+     });
     itemsContainer.innerHTML = itemsHTML;
 }
 render();
 // add to basket function - item appears in the basket with a remove button
+const addButton = document.getElementsByClassName('addButton')
+const cartItems = [];
+function addItemToCart () {
+    cartItems.push(itemObj[this.id]);
+   
+    renderToCart();
+}
+for (let btn of addButton) {
+    btn.addEventListener('click',addItemToCart)
+}
+function renderToCart () {
+     let itemsHTML = "";
+     cartItems.forEach(function (item, index) {
+       itemsHTML += `
+        <div class="itemsElement">
+            <p >${item.name} <span>${item.price}</span></p>
+            <button class="removeButton" id="${index}">-</button>
+        </div>
+        `;
+     });
+     shoppingCart.innerHTML = itemsHTML;
+}
 // remove from basket function - item disappears from the basket
 // total price function - total price of all items in the basket
